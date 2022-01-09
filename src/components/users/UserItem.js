529170b6715820de41a43c9d41919b2fe481dd26
ericksih <erick.saputra08@gmail.com>
2022-01-10 01:47:16 +0700

    Add fitur open user to new Tab

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -13,7 +13,11 @@ const UserItem = ({ user: { login, avatar_url } }) => {
       />
       <h3> {login} </h3>
       <div>
-        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+        <Link
+          to={`/user/${login}`}
+          target='_blank'
+          className='btn btn-dark btn-sm my-1'
+        >
           More
         </Link>
       </div>
